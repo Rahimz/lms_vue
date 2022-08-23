583aa50d132f1a3632a24ae77ca915cb94347451
rahim <rahim.aghareb@gmail.com>
2022-08-23 10:35:35 +0430

    end of video 9

--- src/views/Courses.vue
+++ src/views/Courses.vue
@@ -12,10 +12,23 @@
             <aside class="menu">
               <p class="menu-label">Categories</p>
               <ul class="menu-list">
-                <li><a href="" class="is-active">All courses</a></li>
-                <li><a href="">Programing</a></li>
-                <li><a href="">Design</a></li>
-                <li><a href="">UX</a></li>
+                <li>
+                  <a 
+                    v-bind:class="{'is-active':!activeCategory}"
+                    @click="setActiveCategory(null)"
+                  >
+                  All courses
+                  </a>
+                </li>
+
+                <li
+                 v-for="category in categories"
+                 v-bind:key="category.id"
+                 @click="setActiveCategory(category)"
+                >
+                 <a>{{ category.title }}</a>
+                </li>
+                
               </ul>
             </aside>
           </div>
@@ -61,20 +74,51 @@ import CourseItem from '@/components/CourseItem.vue'
 export default {
     data() {
         return {
-            courses: []
+            courses: [],
+            categories: [],
+            activeCategory: null
+
         };
     },
     components: {
       CourseItem
     },
-    mounted() {
+    async mounted() {
         console.log("mounted");
+        
+        await axios 
+          .get('/api/v1/courses/get_categories/')
+          .then(response => {
+            console.log(response.data)
+            this.categories = response.data
+          })
+
+          this.getCourses()
+        
+        
+    },
+    methods: {
+      setActiveCategory(category) {
+        console.log(category)
+        this.activeCategory = category
+
+        
+          this.getCourses() 
+        
+      },
+      getCourses() {
+        let url = "/api/v1/courses/"
+
+        if (this.activeCategory) {
+          url += '?category_id=' + this.activeCategory.id
+        }
         axios
-            .get("/api/v1/courses/")
+            .get(url)
             .then(response => {
             console.log(response.data);
             this.courses = response.data;
-        });
+        })
+      }
     },
     
 }
