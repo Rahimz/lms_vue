b58025be192d8fbe7464426870d7061445edc43e
rahim <rahim.aghareb@gmail.com>
2023-06-05 11:54:45 +0330

    bug fixed

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import store from './store'
 
 import axios from 'axios'
-axios.defaults.baseURL = 'http://127.0.0.1:8000/api/v1/'
+
+// axios.defaults.baseURL = 'http://127.0.0.1:8000/api/v1/'
+// in production 
+axios.defaults.baseURL = 'http://aghareb.ir/api/v1/'
 
 createApp(App).use(store).use(router, axios).mount('#app')
